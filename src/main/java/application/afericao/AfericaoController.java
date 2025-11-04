059e0fb9d58637b2d7a9660daf1247334f869f08
Isabelly Pimentel <126909881+isabellypimentell@users.noreply.github.com>
2025-11-04 19:44:10 +0000

    Aula04_11

--- src/main/java/application/afericao/AfericaoController.java
+++ src/main/java/application/afericao/AfericaoController.java
@@ -10,34 +10,68 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/afericoes")
+@Tag(name = "Aferições", description = "Gerencia os registros de aferições de sensores ")
 public class AfericaoController {
     @Autowired
     private AfericaoService afericaoService;
 
     @PostMapping
+    @Operation(summary = "Inserir uma nova aferição", description = "Criar um novo registro na base de dados de aferição com os dados enviados.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Aferição criada com sucesso"), 
+        @ApiResponse(responseCode = "400", description = "Dados Invalidos")
+    })
+
     public AfericaoDTO insert(@RequestBody AfericaoInsertDTO novaAfericao) {
         return afericaoService.insert(novaAfericao);
     }
 
     @GetMapping("/{id}")
-    public AfericaoDTO getOne(@PathVariable long id) {
+    @Operation(summary = "Obem dados de uma aferição específica",description = "Retorna os dados de aferição identificadas pelo ID informado.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Aferição encontrada"), 
+        @ApiResponse(responseCode = "400", description = "Aferição não encontrada")
+    })
+    public AfericaoDTO getOne(
+        @Parameter(description = "Id da aferição a ser consultada")
+        @PathVariable long id) {
         return afericaoService.getOne(id);
     }
 
     @GetMapping
+     @Operation(summary = "Obem dados de uma aferição específica",description = "Retorna os dados de aferição identificadas pelo ID informado.")
+    @ApiResponse(responseCode = "200", description = "Aferição encontrada")
     public Iterable<AfericaoDTO> getAll() {
         return afericaoService.getAll();
     }
 
     @PutMapping("/{id}")
-    public AfericaoDTO update(@PathVariable long id, @RequestBody AfericaoInsertDTO novosDados) {
+    @Operation(summary = "Atualiza aferiçõ existente", description = "Altera os dados de uma aferição ja cadastrada, identificada pelo ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Aferição atualizada com sucesso"), 
+        @ApiResponse(responseCode = "400", description = "Aferição não encontrada")
+    })
+    public AfericaoDTO update(
+        @Parameter(description = "ID da aferição a ser atualizada")
+        @PathVariable long id, @RequestBody AfericaoInsertDTO novosDados) {
         return afericaoService.update(id, novosDados);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Remove uma aferição", description = "Exclui permanentemente uma aferição com base no ID informado.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Aferição removida com sucesso "), 
+        @ApiResponse(responseCode = "400", description = "Aferição não encontrada")
+    })
     public void remove(@PathVariable long id) {
         afericaoService.delete(id);
     }
